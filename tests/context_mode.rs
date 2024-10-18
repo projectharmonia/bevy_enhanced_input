40a52a7a4cca97648890f83180569033712ddd26
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-10-19 01:54:59 +0300

    Add tests for context removal and rebuild

--- tests/context_mode.rs
+++ tests/context_mode.rs
@@ -15,8 +15,8 @@ fn exclusive() {
         ActionRecorderPlugin,
     ))
     .add_input_context::<Exclusive>()
-    .record_action::<Consume>()
-    .record_action::<Passthrough>();
+    .record_action::<ExclusiveConsume>()
+    .record_action::<ExclusivePassthrough>();
 
     let entity1 = app.world_mut().spawn(Exclusive).id();
     let entity2 = app.world_mut().spawn(Exclusive).id();
@@ -24,28 +24,28 @@ fn exclusive() {
     app.update();
 
     let mut keys = app.world_mut().resource_mut::<ButtonInput<KeyCode>>();
-    keys.press(Consume::KEY);
-    keys.press(Passthrough::KEY);
+    keys.press(ExclusiveConsume::KEY);
+    keys.press(ExclusivePassthrough::KEY);
 
     app.update();
 
     let recorded = app.world().resource::<RecordedActions>();
 
-    let events = recorded.get::<Consume>(entity1).unwrap();
+    let events = recorded.get::<ExclusiveConsume>(entity1).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
 
-    let events = recorded.get::<Passthrough>(entity1).unwrap();
+    let events = recorded.get::<ExclusivePassthrough>(entity1).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
 
-    let events = recorded.get::<Consume>(entity2).unwrap();
+    let events = recorded.get::<ExclusiveConsume>(entity2).unwrap();
     assert!(
         events.is_empty(),
         "only first entity with the same mappings that consume inputs should receive them"
     );
 
-    let events = recorded.get::<Passthrough>(entity2).unwrap();
+    let events = recorded.get::<ExclusivePassthrough>(entity2).unwrap();
     let event = events.last().unwrap();
     assert_eq!(
         event.state,
@@ -64,8 +64,8 @@ fn shared() {
         ActionRecorderPlugin,
     ))
     .add_input_context::<Shared>()
-    .record_action::<Consume>()
-    .record_action::<Passthrough>();
+    .record_action::<SharedConsume>()
+    .record_action::<SharedPassthrough>();
 
     let entity1 = app.world_mut().spawn(Shared).id();
     let entity2 = app.world_mut().spawn(Shared).id();
@@ -73,28 +73,109 @@ fn shared() {
     app.update();
 
     let mut keys = app.world_mut().resource_mut::<ButtonInput<KeyCode>>();
-    keys.press(Consume::KEY);
-    keys.press(Passthrough::KEY);
+    keys.press(SharedConsume::KEY);
+    keys.press(SharedPassthrough::KEY);
 
     app.update();
 
     let recorded = app.world().resource::<RecordedActions>();
 
-    let events = recorded.get::<Consume>(entity1).unwrap();
+    let events = recorded.get::<SharedConsume>(entity1).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
 
-    let events = recorded.get::<Passthrough>(entity1).unwrap();
+    let events = recorded.get::<SharedPassthrough>(entity1).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
 
-    let events = recorded.get::<Consume>(entity2).unwrap();
+    let events = recorded.get::<SharedConsume>(entity2).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
 
-    let events = recorded.get::<Passthrough>(entity2).unwrap();
+    let events = recorded.get::<SharedPassthrough>(entity2).unwrap();
     let event = events.last().unwrap();
     assert_eq!(event.state, ActionState::Fired);
+}
+
+#[test]
+fn removal() {
+    let mut app = App::new();
+    app.add_plugins((
+        MinimalPlugins,
+        InputPlugin,
+        EnhancedInputPlugin,
+        ActionRecorderPlugin,
+    ))
+    .add_input_context::<Exclusive>()
+    .add_input_context::<Shared>()
+    .record_action::<ExclusiveConsume>()
+    .record_action::<SharedConsume>();
+
+    let entity = app.world_mut().spawn((Exclusive, Shared)).id();
+
+    app.update();
+
+    let mut keys = app.world_mut().resource_mut::<ButtonInput<KeyCode>>();
+    keys.press(ExclusiveConsume::KEY);
+    keys.press(SharedConsume::KEY);
+
+    app.update();
+
+    app.world_mut()
+        .entity_mut(entity)
+        .remove::<Exclusive>()
+        .remove::<Shared>();
+
+    app.update();
+
+    let recorded = app.world().resource::<RecordedActions>();
+
+    let events = recorded.get::<ExclusiveConsume>(entity).unwrap();
+    let [event] = events.try_into().unwrap();
+    assert!(event.transition.is_completed());
+
+    let events = recorded.get::<SharedConsume>(entity).unwrap();
+    let [event] = events.try_into().unwrap();
+    assert!(event.transition.is_completed());
+}
+
+#[test]
+fn rebuild() {
+    let mut app = App::new();
+    app.add_plugins((
+        MinimalPlugins,
+        InputPlugin,
+        EnhancedInputPlugin,
+        ActionRecorderPlugin,
+    ))
+    .add_input_context::<Exclusive>()
+    .add_input_context::<Shared>()
+    .record_action::<ExclusiveConsume>()
+    .record_action::<SharedConsume>();
+
+    let entity = app.world_mut().spawn((Exclusive, Shared)).id();
+
+    app.update();
+
+    let mut keys = app.world_mut().resource_mut::<ButtonInput<KeyCode>>();
+    keys.press(ExclusiveConsume::KEY);
+    keys.press(SharedConsume::KEY);
+
+    app.update();
+
+    app.world_mut().trigger(RebuildInputContexts);
+
+    app.update();
+
+    let recorded = app.world().resource::<RecordedActions>();
+
+    let events = recorded.get::<ExclusiveConsume>(entity).unwrap();
+    let [event] = events.try_into().unwrap();
+    assert!(event.transition.is_completed());
+
+    let events = recorded.get::<SharedConsume>(entity).unwrap();
+    let [event] = events.try_into().unwrap();
+    assert!(event.transition.is_completed());
 }
 
 #[derive(Debug, Component)]
@@ -105,8 +186,9 @@ impl InputContext for Exclusive {
 
     fn context_instance(_world: &World, _entity: Entity) -> ContextInstance {
         let mut ctx = ContextInstance::default();
-        ctx.bind::<Consume>().with(Consume::KEY);
-        ctx.bind::<Passthrough>().with(Passthrough::KEY);
+        ctx.bind::<ExclusiveConsume>().with(ExclusiveConsume::KEY);
+        ctx.bind::<ExclusivePassthrough>()
+            .with(ExclusivePassthrough::KEY);
         ctx
     }
 }
@@ -119,24 +201,40 @@ impl InputContext for Shared {
 
     fn context_instance(_world: &World, _entity: Entity) -> ContextInstance {
         let mut ctx = ContextInstance::default();
-        ctx.bind::<Consume>().with(Consume::KEY);
-        ctx.bind::<Passthrough>().with(Passthrough::KEY);
+        ctx.bind::<SharedConsume>().with(SharedConsume::KEY);
+        ctx.bind::<SharedPassthrough>().with(SharedPassthrough::KEY);
         ctx
     }
 }
 
 #[derive(Debug, InputAction)]
 #[input_action(dim = Bool, consume_input = true)]
-struct Consume;
+struct ExclusiveConsume;
 
-impl Consume {
+impl ExclusiveConsume {
     const KEY: KeyCode = KeyCode::KeyA;
 }
 
 #[derive(Debug, InputAction)]
 #[input_action(dim = Bool, consume_input = false)]
-struct Passthrough;
+struct ExclusivePassthrough;
 
-impl Passthrough {
+impl ExclusivePassthrough {
     const KEY: KeyCode = KeyCode::KeyB;
+}
+
+#[derive(Debug, InputAction)]
+#[input_action(dim = Bool, consume_input = true)]
+struct SharedConsume;
+
+impl SharedConsume {
+    const KEY: KeyCode = KeyCode::KeyC;
+}
+
+#[derive(Debug, InputAction)]
+#[input_action(dim = Bool, consume_input = false)]
+struct SharedPassthrough;
+
+impl SharedPassthrough {
+    const KEY: KeyCode = KeyCode::KeyD;
 }
