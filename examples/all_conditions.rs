b9ea13a080bff633e600f707056df44a3e412c0e
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-04-05 19:55:02 +0300

    Fix logging in `all_conditions` (#66)

--- examples/all_conditions.rs
+++ examples/all_conditions.rs
@@ -7,7 +7,7 @@ use bevy_enhanced_input::prelude::*;
 fn main() {
     // Setup logging to display triggered events.
     let mut log_plugin = LogPlugin::default();
-    log_plugin.filter += ",bevy_enhanced_input::input_action=trace";
+    log_plugin.filter += ",bevy_enhanced_input::action_map=trace";
 
     App::new()
         .add_plugins((
