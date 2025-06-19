87dfd6ec141345fdfb327da7ea4d3299fa5af65e
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-06-19 10:18:21 +0300

    Apply clippy suggestions
    We switched to typed `Actions::value` and `into` conversion no longer
    needed.

--- tests/accumulation.rs
+++ tests/accumulation.rs
@@ -21,7 +21,7 @@ fn max_abs() {
     app.update();
 
     let actions = app.world().get::<Actions<Test>>(entity).unwrap();
-    assert_eq!(actions.value::<MaxAbs>().unwrap(), Vec2::Y.into());
+    assert_eq!(actions.value::<MaxAbs>().unwrap(), Vec2::Y);
 }
 
 #[test]
@@ -45,7 +45,7 @@ fn cumulative() {
     let actions = app.world().get::<Actions<Test>>(entity).unwrap();
     assert_eq!(
         actions.value::<Cumulative>().unwrap(),
-        Vec2::ZERO.into(),
+        Vec2::ZERO,
         "up and down should cancel each other"
     );
 }
