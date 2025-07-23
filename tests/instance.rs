a132dae6d8973660b0ed584f778f40883ec34cde
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-07-23 20:55:08 +0300

    Support entity disabling (#143)
    * Rename `instances` into `instance`
    
    * Add support for disabled entities
    
    * Fix lib compilation

--- tests/instance.rs
+++ tests/instance.rs
@@ -1,4 +1,4 @@
-use bevy::{input::InputPlugin, prelude::*};
+use bevy::{ecs::entity_disabling::Disabled, input::InputPlugin, prelude::*};
 use bevy_enhanced_input::prelude::*;
 use test_log::test;
 
@@ -55,6 +55,46 @@ fn invalid_hierarchy() {
     ));
 
     app.update();
+}
+
+#[test]
+fn disabled() {
+    let mut app = App::new();
+    app.add_plugins((MinimalPlugins, InputPlugin, EnhancedInputPlugin))
+        .add_input_context::<TestContext>()
+        .finish();
+
+    let disabled = app
+        .world_mut()
+        .spawn((
+            TestContext,
+            Disabled,
+            actions!(TestContext[(Action::<Test>::new(), bindings![Test::KEY])]),
+        ))
+        .id();
+
+    let with_disabled_action = app
+        .world_mut()
+        .spawn((
+            TestContext,
+            actions!(TestContext[(Action::<Test>::new(), Disabled, bindings![Test::KEY])]),
+        ))
+        .id();
+
+    app.update();
+
+    app.world_mut()
+        .resource_mut::<ButtonInput<KeyCode>>()
+        .press(Test::KEY);
+
+    app.world_mut().add_observer(|_: Trigger<Fired<Test>>| {
+        panic!("action shouldn't trigger");
+    });
+
+    app.update();
+
+    app.world_mut().despawn(disabled);
+    app.world_mut().despawn(with_disabled_action);
 }
 
 #[derive(Component)]
